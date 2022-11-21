72f5002bf10286b63a5891bbfaba4f139cfaf088
Lincoln Mitchell <lincoln.mitchell@merkleinc.com.au>
2022-11-21 13:20:32 +1100

    added card to class names

--- styles/theme/components/card.ts
+++ styles/theme/components/card.ts
@@ -4,10 +4,8 @@ import { ComponentStyleConfig } from '@chakra-ui/react'
 const Card: ComponentStyleConfig = {
   variants: {
     primary: {
-      bg: 'btn-primary-default-background-color',
-      color: 'btn-primary-default-text-color',
-      borderColor: 'btn-primary-default-border-color',
-      iconColor:"btn-primary-default-icon-color"
+      bg: 'card-primary-default-background-color',
+      color: 'card-primary-default-text-color',
     },
   },
   defaultProps: {
